c03f9a2daf4a12d06bdd517c1b531643f441e57e
Paul <guzypaul@gmail.com>
2021-08-23 01:34:17 +0300

    created structure

--- task03/src/main/java/by/guzypaul/arraymatrix/service/matrix/impl/MatrixSubstractionImpl.java
+++ task03/src/main/java/by/guzypaul/arraymatrix/service/matrix/impl/MatrixSubstractionImpl.java
@@ -7,7 +7,17 @@ import by.guzypaul.arraymatrix.service.matrix.MatrixSubtraction;
 public class MatrixSubstractionImpl implements MatrixSubtraction {
     @Override
     public MatrixEntity substract(MatrixEntity matrixOne, MatrixEntity matrixTwo) throws MatrixException {
-        //TODO body
-        return null;
+
+        int verticalSize = matrixOne.getVerticalSize();
+        int horizontalSize = matrixTwo.getHorizontalSize();
+
+        MatrixEntity result = new MatrixEntity(verticalSize, horizontalSize);
+        for (int i = 0; i < result.getVerticalSize(); i++) {
+            for (int j = 0; j < result.getHorizontalSize(); j++) {
+                result.setElement(i, j, matrixOne.getElement(i, j) - matrixTwo.getElement(i, j));
+            }
+        }
+
+        return result;
     }
 }
--- task03/src/main/java/by/guzypaul/arraymatrix/service/matrix/impl/MatrixSumImpl.java
+++ task03/src/main/java/by/guzypaul/arraymatrix/service/matrix/impl/MatrixSumImpl.java
@@ -6,9 +6,18 @@ import by.guzypaul.arraymatrix.service.matrix.MatrixSum;
 
 public class MatrixSumImpl implements MatrixSum {
     @Override
-    public MatrixEntity sum(MatrixEntity p, MatrixEntity q) throws MatrixException {
+    public MatrixEntity sum(MatrixEntity matrixOne, MatrixEntity matrixTwo) throws MatrixException {
 
-        //TODO body
-        return null;
+        int verticalSize = matrixOne.getVerticalSize();
+        int horizontalSize = matrixTwo.getHorizontalSize();
+
+        MatrixEntity result = new MatrixEntity(verticalSize, horizontalSize);
+        for (int i = 0; i < result.getVerticalSize(); i++) {
+            for (int j = 0; j < result.getHorizontalSize(); j++) {
+                result.setElement(i, j, matrixOne.getElement(i, j) + matrixTwo.getElement(i, j));
+            }
+        }
+
+        return result;
     }
 }
