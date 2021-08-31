89011a517201ea7c4f65961b8f74886e24e0a476
Paul <guzypaul@gmail.com>
2021-08-31 19:11:33 +0300

    created structure

--- task04/src/main/java/by/guzypaul/classes/entity/Country.java
+++ task04/src/main/java/by/guzypaul/classes/entity/Country.java
@@ -36,88 +36,9 @@ public class Country {
         return regionsList;
     }
 
-    static String strInput() {
-        Scanner sc = new Scanner(System.in);
-        if (sc.hasNextLine()) {
-            strInput = sc.nextLine();
-        } else {
-            System.out.println("Недопустимый ввод");
-            strInput();
-        }
-        return strInput;
-    }
-
-    static int numInput() {
-        Scanner sc = new Scanner(System.in);
-        if (sc.hasNextInt()) {
-            numInput = sc.nextInt();
-        } else {
-            System.out.println("Недопустимый ввод");
-            numInput();
-        }
-        return numInput;
-    }
-
-    static double doublInput() {
-        Scanner sc = new Scanner(System.in);
-        if (sc.hasNextDouble()) {
-            doublInput = sc.nextDouble();
-        } else {
-            System.out.println("Недопустимый ввод");
-            doublInput();
-        }
-        return doublInput;
-    }
-
-    static Country addCountry() {
+ /*   static Country addCountry() {
         Country country = new Country(null,null,null);
         country.regionsList.add(Region.addRegion());
         return country;
-    }
-
+    }*/
 }
-
-
-
-
-
-
-
-/*class Program {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Country country = new Country(Country.strInput(), Country.strInput());
-        while (true) {
-            System.out.println("Выберите команду:\n" +
-                    "0 - выход\n" +
-                    "1 - добавить область\n" +
-                    "2 - вывести на консоль столицу\n" +
-                    "3 - количество областей\n" +
-                    "4 - площадь\n" +
-                    "5 - областные центры");
-
-            int choice = Country.numInput();
-            if (choice == 0) {
-                break;
-            }
-            if (choice < 1 || choice > 5) {
-                System.out.println("Повторите ввод");
-                continue;
-            }
-            switch (choice) {
-                case 1:
-                    country.addCountry();
-                    break;
-                case 2:
-                    System.out.println("Страна: " + country.getCountry());
-                    System.out.println("Столица: " + country.getCapital());
-                    break;
-                case 3:
-                    System.out.println("Кол - во областей: " + country.getRegionList().size());
-                    break;
-                case 4:
-                    //System.out.println("Площадь всех областей: " + country.squareRegions());
-            }
-        }
-    }
-}*/
