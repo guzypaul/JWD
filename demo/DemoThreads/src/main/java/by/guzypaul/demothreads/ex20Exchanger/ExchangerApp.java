d3c87660592f990262a3a537531fdc19189810f1
Paul <guzypaul@gmail.com>
2021-09-17 17:05:28 +0300

    DEMO Threads

---
+++ demo/DemoThreads/src/main/java/by/guzypaul/demothreads/ex20Exchanger/ExchangerApp.java
@@ -0,0 +1,27 @@
+package by.guzypaul.demothreads.ex20Exchanger;
+
+import java.util.concurrent.Exchanger;
+
+public class ExchangerApp {
+
+    private static int n = 1;
+    public static void main(String[] args) {
+
+        Exchanger<String> ex = new Exchanger();
+
+        for (int i = 1; i <= n; i++) {
+
+            Thread t1 = new Thread(new PutThread(ex));
+            Thread t2 = new Thread(new GetThread(ex));
+
+            t1.setName("Поток Put " + i);
+            t2.setName("Поток Get " + i);
+
+            t1.start();
+            t2.start();
+
+        }
+    }
+
+}
+
