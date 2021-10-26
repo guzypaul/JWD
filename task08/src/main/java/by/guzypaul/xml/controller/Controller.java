5798942692b30683fb26f497d0d69bbac30f31a8
Paul <guzypaul@gmail.com>
2021-10-26 15:37:22 +0300

    html added

---
+++ task08/src/main/java/by/guzypaul/xml/controller/Controller.java
@@ -0,0 +1,18 @@
+package by.guzypaul.xml.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/controller")
+public class Controller extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><body><h1>Hello!</h1></body></html>");
+    }
+}
